b0868486e2990853bdf1691154f51eb51fb9a6bb
mfuadfakhruzzaki <mfuadfakhruzzaki@students.undip.ac.id>
2024-07-23 11:27:56 +0700

    Co-authored-by: neztanez <neztanez@users.noreply.github.com> Co-authored-by: radityawisnucn <radityawisnucn@users.noreply.github.com>

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,33 @@
 import { SafeAreaView, Text, View, StyleSheet } from "react-native";
+import { useFonts, Outfit_400Regular } from "@expo-google-fonts/outfit";
 
 export default function Index() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.teks}>Halo kontol!</Text>
+    <SafeAreaView style={styles.root}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Project Management</Text>
+        <Text style={styles.subtitle}>Project Management</Text>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
+    fontFamily: "Outfit_400Regular",
     display: "flex",
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
-  teks: {
+  container: {
+    padding: 16,
+  },
+  title: {
     fontSize: 20,
     fontWeight: "bold",
-    fontFamily: "sans-serif",
+    fontFamily: "Outfit_400Regular",
+  },
+  subtitle: {
+    fontSize: 16,
+    fontFamily: "Outfit_400Regular",
   },
 });
